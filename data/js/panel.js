046f6e8869cc3bd7cec5691f921693dc04ff717e
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2014-11-26 01:36:01 +0200

    up

--- data/js/panel.js
+++ data/js/panel.js
@@ -2,11 +2,10 @@ var text;
 var textArea = document.getElementById("edit-box");
 var button 	 = document.getElementById("btn-translate");
 var parag	 = document.getElementById("result");
-var option   = document.getElementById("lan");
 
 button.addEventListener("click", function(event){
 	  text = textArea.value;
-	  self.port.emit("translate", text, option.value);
+	  self.port.emit("translate", text);
 });
 
 self.port.on("send", function(translated_text){
